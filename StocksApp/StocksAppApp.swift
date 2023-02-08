d424ece9a9d94c6c7e73827f242b947527309a80
andreia1239 <andreia@apadmi.com>
2023-02-08 17:45:23 +0000

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Andrei Avram on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
